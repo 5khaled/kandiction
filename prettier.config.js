d97224064f431f1a0c85a900300f061756797230
vwh <vwhe@proton.me>
2024-12-22 20:42:55 +0300

    feat: added prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "none",
+  tabWidth: 2,
+};
+
+export default config;
