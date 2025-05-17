405f0a596702e27a5b29976444f4fa4999bf3155
5Khaled <Khaled2shboul@gmail.com>
2025-05-17 22:21:05 +0300

    feat: update main bg gradient colors

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div
       className="min-h-dvh flex flex-col bg-gradient-to-br 
-    from-emerald-600 via-cyan-600 to-teal-600 selection:bg-cyan-100 selection:text-cyan-700 
-    dark:from-cyan-800 dark:via-sky-950 dark:to-zinc-900 dark:selection:bg-cyan-700 dark:selection:text-cyan-100"
+    from-emerald-600 via-cyan-600 to-cyan-600 selection:bg-cyan-100 selection:text-cyan-700 
+    dark:from-cyan-950 dark:via-sky-950 dark:to-cyan-950 dark:selection:bg-cyan-700 dark:selection:text-cyan-100"
     >
       <Header navOpen={navOpen} setNavOpen={setNavOpen} />
       <main
