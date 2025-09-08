edf22d1aac19efe6fa8ac2b135652deb5cbee9bc
5khaled <khaled2shboul@gmail.com>
2025-09-08 06:15:37 +0300

    feat: add go back button to not found page

--- app/routes/not-found.tsx
+++ app/routes/not-found.tsx
@@ -1,4 +1,6 @@
-import type { Route } from "./+types/home";
+import { Link } from "react-router";
+import type { Route } from "./+types/not-found";
+import { Button } from "~/components/shadcn/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +13,12 @@ export function meta({}: Route.MetaArgs) {
 }
 const NotFound = () => {
   return (
-    <div>
+    <div className={`h-dvh flex flex-col items-center justify-center`}>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
+      <Button asChild variant={"secondary"} className="my-4">
+        <Link to="/">Go Back</Link>
+      </Button>
     </div>
   );
 };
