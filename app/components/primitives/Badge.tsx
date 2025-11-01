4f2ac76a8161ce70cd3a27d5ac92b471bec1715d
5khaled <khaled2shboul@gmail.com>
2025-11-01 04:32:14 +0300

    feat(Badge): add kbd variant with keyboard interaction support

--- app/components/primitives/Badge.tsx
+++ app/components/primitives/Badge.tsx
@@ -1,4 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
+import { useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { cn } from "~/lib/utils";
 
 const badgeVariants = cva(`flex items-center rounded text-nowrap`, {
@@ -8,6 +10,7 @@ const badgeVariants = cva(`flex items-center rounded text-nowrap`, {
       primary: "bg-primary text-foreground",
       outline:
         "bg-input text-secondary-foreground outline outline-border -outline-offset-1",
+      kbd: "bg-border text-secondary-foreground shadow-[inset_0px_-2px_0_0px_rgba(0,0,0,0.25)]",
     },
     size: {
       sm: "[&>svg]:size-2.5 gap-0.5 py-px px-1 text-[0.625rem]",
@@ -29,12 +32,46 @@ export default function Badge({
   className,
   ...props
 }: React.ComponentProps<"span"> & VariantProps<typeof badgeVariants>) {
+  const [isHolding, setisHolding] = useState(false);
+
+  if (variant === "kbd") {
+    useHotkeys(
+      String(children),
+      () => {
+        setisHolding(true);
+      },
+      {
+        preventDefault: true,
+        enableOnFormTags: true,
+        keydown: true,
+      },
+    );
+
+    useHotkeys(
+      String(children),
+      () => {
+        setisHolding(false);
+      },
+      {
+        preventDefault: true,
+        enableOnFormTags: true,
+        keyup: true,
+      },
+    );
+  }
+
   return (
     <span
       className={cn(badgeVariants({ variant, size, className }))}
       {...props}
     >
-      {children}
+      {variant === "kbd" ? (
+        <kbd className={cn(`-translate-y-px`, isHolding ? "text-primary" : "")}>
+          {children}
+        </kbd>
+      ) : (
+        children
+      )}
     </span>
   );
 }
