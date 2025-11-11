4a61d0be9ae16fb27f0b54a7c016ede265db8cb5
5khaled <khaled2shboul@gmail.com>
2025-11-11 21:49:44 +0300

    feat: add contact section & small spacing adjustment

--- app/routes/landing/landing-page.tsx
+++ app/routes/landing/landing-page.tsx
@@ -3,7 +3,7 @@ import { cn } from "~/lib/utils";
 
 import KandictionIcon from "~/components/icons/kandiction_logo";
 
-import { Twitter } from "lucide-react";
+import { Mail, Twitter } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -18,16 +18,7 @@ export function meta({}: Route.MetaArgs) {
 export default function MainLayout() {
   return (
     <>
-      <header className="fixed top-0 w-full flex flex-row-reverse">
-        <a
-          href="https://x.com/5kh4led"
-          target="_blank"
-          title="I'm not active on X, but a follow is appreciated."
-          className="group p-3"
-        >
-          <Twitter className="text-secondary-foreground group-hover:text-foreground fill-current size-5" />
-        </a>
-      </header>
+      <header className="fixed top-0 w-full flex flex-row-reverse"></header>
       <main
         className={cn(
           `container h-screen flex flex-col items-center sm:justify-center mx-auto max-sm:mt-24 px-8 space-y-8`,
@@ -63,12 +54,10 @@ export default function MainLayout() {
       </main>
       <section
         className={cn(
-          `container flex flex-col items-center space-y-8 mx-auto px-8 mb-32`,
+          `container flex flex-col items-center gap-4 mx-auto px-8 mb-32`,
         )}
       >
-        <h1 className="text-2xl md:text-4xl font-medium">
-          What is Kandiction?
-        </h1>
+        <h1 className="text-xl md:text-2xl font-medium">What is Kandiction?</h1>
         <p className="text-center text-pretty text-secondary-foreground max-w-[75ch]">
           Kandiction is a kanji reference I’m building to make looking up
           characters simple and actually useful. You can check meanings,
@@ -78,13 +67,41 @@ export default function MainLayout() {
           — more will come as it grows.
         </p>
       </section>
-      <footer className="p-4 w-full flex justify-center">
+
+      <footer className="p-4 w-full flex flex-col items-center gap-8">
+        <section className="flex flex-col gap-4">
+          <h1 className="self-center text-xl md:text-2xl font-medium">
+            Reach out
+          </h1>
+          <div className="flex justify-center flex-wrap gap-4">
+            <a
+              href="https://x.com/5kh4led"
+              target="_blank"
+              title="I'm not active on X, but a follow is appreciated."
+              className="group flex items-center gap-1 text-secondary-foreground hover:text-foreground transition-all"
+            >
+              <Twitter className="fill-current size-4" />
+              <span className="text-sm group-hover:underline">@5kh4led</span>
+            </a>
+            <a
+              href="mailto:khaled2shboul@gmail.com"
+              target="_blank"
+              title="I'm not active on X, but a follow is appreciated."
+              className="group flex items-center gap-1 text-secondary-foreground hover:text-foreground transition-all"
+            >
+              <Mail className="size-4" />
+              <span className="text-sm group-hover:underline">
+                khaled2shboul@gmail.com
+              </span>
+            </a>
+          </div>
+        </section>
         <div className="text-secondary-foreground font-light">
           Made by{" "}
           <a
             href="https://kald.dev"
             target="_blank"
-            className="hover:underline"
+            className="pointer-coarse:underline pointer-fine:hover:underline"
           >
             KALD
           </a>
