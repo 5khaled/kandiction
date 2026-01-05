d918ac876b45fa81941e5df55e74f8f6cb7f574b
5khaled <khaled2shboul@gmail.com>
2026-01-05 17:26:29 +0300

    feat: refactor KanjiMeanings component to use Crate for layout

--- app/routes/kanji/components/kanjiMeanings.tsx
+++ app/routes/kanji/components/kanjiMeanings.tsx
@@ -1,4 +1,5 @@
-import { Book, BookOpenText } from "lucide-react";
+import { BookOpenText } from "lucide-react";
+import { Crate } from "~/components/primitives/Crate";
 import { cn } from "~/lib/utils";
 
 interface KanjiMeaningsProps extends React.ComponentProps<"section"> {
@@ -11,32 +12,33 @@ export default function KanjiMeanings({
   ...props
 }: KanjiMeaningsProps) {
   return (
-    <section
-      className={cn(`flex flex-col border rounded bg-card`, className)}
-      {...props}
-    >
-      <h2 className="flex items-center border-b gap-2 px-3 py-2">
-        <BookOpenText className="size-4 text-primary fill-current/25" />
-        <span className="font-medium">Meanings</span>
-      </h2>
-      <div className="m-3 flex border bg-background rounded-xl min-h-17.75 sm:max-h-20 overflow-hidden">
-        {meanings && meanings.length > 0 ? (
-          <ul className="list-none flex flex-wrap content-start gap-0.75 p-0.75 overflow-y-scroll scrollbar">
-            {meanings.map((meaning, index) => (
-              <li
-                className="flex items-center rounded text-secondary-foreground bg-card border border-disabled border-dashed px-3 h-7.5"
-                key={index}
-              >
-                {meaning}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span className="grow flex items-center justify-center text-disabled">
-            No meanings available.
-          </span>
-        )}
-      </div>
-    </section>
+    <Crate className={cn(className)} {...props}>
+      <Crate.Title>
+        <BookOpenText />
+        Meanings
+      </Crate.Title>
+      <Crate.Content>
+        <Crate.Group>
+          <Crate.GroupContent>
+            {meanings && meanings.length > 0 ? (
+              <ul className="list-none flex flex-wrap content-start gap-1 p-1 overflow-y-scroll scrollbar">
+                {meanings.map((meaning, index) => (
+                  <li
+                    className="font-light flex items-center rounded text-secondary-foreground bg-card px-3 h-7.5"
+                    key={index}
+                  >
+                    {meaning}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <span className="grow flex items-center justify-center text-disabled">
+                No meanings available.
+              </span>
+            )}
+          </Crate.GroupContent>
+        </Crate.Group>
+      </Crate.Content>
+    </Crate>
   );
 }
